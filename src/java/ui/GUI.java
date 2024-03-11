81ce6402cbe64daf534aea2fe0886d73d4b3fbb6
MohamedGacha <tafaouk5@gmail.com>
2024-03-11 14:07:30 +0100

    Add Gui

---
+++ src/java/ui/GUI.java
@@ -0,0 +1,35 @@
+package java.ui;
+
+import javax.swing.*;
+
+public class GUI {
+
+    private JButton New;
+    private JButton action1;
+    private JToolBar ActionBar;
+    private JButton Action3;
+    private JButton Action2;
+    private JButton Open;
+    private JButton Save;
+    private JButton Server;
+    private JButton Exit;
+    private JButton Action4;
+    private JButton ZoomIn;
+    private JSpinner spinner1;
+    private JButton ZoomOut;
+    private JButton Mouse;
+    private JButton Form1;
+    private JButton Form2;
+    private JButton Form3;
+    private JButton Form4;
+    private JButton Form5;
+    private JButton Form6;
+    private JButton button1;
+    private JSpinner spinner3;
+    private JSlider slider1;
+    private JRadioButton radioButton1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
